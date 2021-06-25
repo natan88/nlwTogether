f6a6dee83da8fea0fc3ca484938570dc4faaab5a
Natanael <bsbinfor@gmail.com>
2021-06-24 21:36:56 -0300

    fix response type create tag

--- src/controllers/CreateTagController.ts
+++ src/controllers/CreateTagController.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from "express";
 import { CreateTagService } from "../services/CreateTagService";
+import { IResponseSuccess } from "../util/IResponse";
 
 export class CreateTagController {
-  async handle(request: Request, response: Response) {
+  async handle(request: Request, response: Response<IResponseSuccess>) {
     const { name } = request.body;
 
     const createTagService = new CreateTagService();
     const tag = await createTagService.execute(name);
-    return response.json(tag);
+    return response.status(201).json({
+      success: true,
+      statusCode: 201,
+      payload: tag,
+    });
   }
 }
