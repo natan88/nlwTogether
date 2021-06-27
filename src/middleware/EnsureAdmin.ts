08126f1d86cddb1132355441d70b8f82cb8ee030
Natanael <bsbinfor@gmail.com>
2021-06-27 20:18:04 -0300

    Update Middleware EnsureAdmin fixed #1

--- src/middleware/EnsureAdmin.ts
+++ src/middleware/EnsureAdmin.ts
@@ -1,10 +1,18 @@
 import { Request, Response, NextFunction, response } from "express";
+import { UserRepositories } from "../repositories/UserRepositories";
+import { getCustomRepository } from "typeorm";
 
-export function EnsureAdmin(req: Request, res: Response, next: NextFunction) {
-  const isAdmin = true;
+export async function EnsureAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const { user_id } = request;
+  const usersRepository = getCustomRepository(UserRepositories);
+  const { admin } = await usersRepository.findOne(user_id);
 
-  if (isAdmin) return next();
-  return res.status(401).json({
-    error: "Unauthorized",
+  if (admin) return next();
+  return response.status(401).json({
+    error: "Unauthorized"
   });
 }
